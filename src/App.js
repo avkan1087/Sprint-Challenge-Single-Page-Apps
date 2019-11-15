1c134e7defcb29d623cb505f1d730b9d51388a06
avi <avikan1087@gmail.com>
2019-11-15 14:06:50 -0500

    mvp: done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+import SearchForm from "./components/SearchForm";
+import { Route } from"react-router-dom";
+
 
 
 export default function App() {
   return (
+
+
     <main>
       <Header />
+      <Route exact path="/Search" component={SearchForm}/>
+      <Route exact path ="/" component={WelcomePage}/>
+      <Route exact path="/Characters" component={CharacterList}/>
     </main>
   );
 }
+
+
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CharacterList from "./CharacterList";
+import {Link} from "react-router-dom";
+
 
 export default function SearchForm() {
- 
+
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+  
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
-}
+    <div >
+      <form > 
+        <input
+        id="name" type="text" name="textfield" placeholder="Search" value={query} onChange={handleInputChange}/>
+
+        <Link to="/"><button>Home</button></Link>
+      </form>
+      <CharacterList data={data} setData={setData} query={query} />
+  </div>
+)}
